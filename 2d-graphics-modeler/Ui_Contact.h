7739a2732106e5d86bd124b94c22ba79c8b693f1
Dogtor-Hegcht <68541916+Dogtor-Hegcht@users.noreply.github.com>
2020-07-22 19:21:06 -0700

    Update Ui_Contact.h

--- 2d-graphics-modeler/Ui_Contact.h
+++ 2d-graphics-modeler/Ui_Contact.h
@@ -1,61 +1,238 @@
 
+/********************************************************************************
+** Form generated from reading UI file 'contact.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
 #ifndef UI_CONTACT_H
 #define UI_CONTACT_H
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QDialog>
+#include <QtWidgets/QGroupBox>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
 
-class Ui_Contact
+class Ui_contact
 {
 public:
+    QGroupBox *groupBox;
     QLabel *label;
+    QWidget *layoutWidget;
+    QVBoxLayout *verticalLayout;
     QLabel *label_2;
+    QLabel *label_4;
+    QWidget *layoutWidget1;
+    QVBoxLayout *verticalLayout_2;
+    QLabel *label_3;
+    QLabel *label_5;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout_3;
+    QLabel *label_8;
+    QLabel *label_12;
+    QWidget *widget1;
+    QVBoxLayout *verticalLayout_4;
+    QLabel *label_6;
+    QLabel *label_13;
+    QWidget *widget2;
+    QVBoxLayout *verticalLayout_5;
+    QLabel *label_7;
+    QLabel *label_14;
+    QWidget *widget3;
+    QVBoxLayout *verticalLayout_6;
+    QLabel *label_9;
+    QLabel *label_15;
+    QWidget *widget4;
+    QVBoxLayout *verticalLayout_7;
+    QLabel *label_10;
+    QLabel *label_16;
+    QWidget *widget5;
+    QVBoxLayout *verticalLayout_8;
+    QLabel *label_11;
+    QLabel *label_17;
 
-    void setupUi(QDialog *Contact)
+    void setupUi(QWidget *contact)
     {
-        if (Contact->objectName().isEmpty())
-            Contact->setObjectName(QString::fromUtf8("Contact"));
-        Contact->resize(472, 629);
-        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
-        sizePolicy.setHorizontalStretch(0);
-        sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(Contact->sizePolicy().hasHeightForWidth());
-        Contact->setSizePolicy(sizePolicy);
-        Contact->setMinimumSize(QSize(472, 629));
-        Contact->setMaximumSize(QSize(472, 629));
-        Contact->setBaseSize(QSize(472, 629));
-        label = new QLabel(Contact);
+        if (contact->objectName().isEmpty())
+            contact->setObjectName(QString::fromUtf8("contact"));
+        contact->resize(400, 300);
+        groupBox = new QGroupBox(contact);
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
+        groupBox->setGeometry(QRect(10, 0, 371, 301));
+        label = new QLabel(groupBox);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(100, 20, 281, 281));
-      
-        label->setScaledContents(true);
-        label_2 = new QLabel(Contact);
+        label->setGeometry(QRect(80, -10, 221, 201));
+        layoutWidget = new QWidget(groupBox);
+        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setGeometry(QRect(20, 160, 79, 36));
+        verticalLayout = new QVBoxLayout(layoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        label_2 = new QLabel(layoutWidget);
         label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setGeometry(QRect(0, 340, 481, 291));
-        label_2->setScaledContents(true);
-        label_2->setAlignment(Qt::AlignCenter);
-        label_2->setOpenExternalLinks(true);
 
-        retranslateUi(Contact);
+        verticalLayout->addWidget(label_2);
 
-        QMetaObject::connectSlotsByName(Contact);
+        label_4 = new QLabel(layoutWidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        verticalLayout->addWidget(label_4);
+
+        layoutWidget1 = new QWidget(groupBox);
+        layoutWidget1->setObjectName(QString::fromUtf8("layoutWidget1"));
+        layoutWidget1->setGeometry(QRect(130, 160, 97, 36));
+        verticalLayout_2 = new QVBoxLayout(layoutWidget1);
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        label_3 = new QLabel(layoutWidget1);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        verticalLayout_2->addWidget(label_3);
+
+        label_5 = new QLabel(layoutWidget1);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+
+        verticalLayout_2->addWidget(label_5);
+
+        widget = new QWidget(groupBox);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(260, 160, 90, 36));
+        verticalLayout_3 = new QVBoxLayout(widget);
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        label_8 = new QLabel(widget);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+
+        verticalLayout_3->addWidget(label_8);
+
+        label_12 = new QLabel(widget);
+        label_12->setObjectName(QString::fromUtf8("label_12"));
+
+        verticalLayout_3->addWidget(label_12);
+
+        widget1 = new QWidget(groupBox);
+        widget1->setObjectName(QString::fromUtf8("widget1"));
+        widget1->setGeometry(QRect(20, 200, 82, 50));
+        verticalLayout_4 = new QVBoxLayout(widget1);
+        verticalLayout_4->setObjectName(QString::fromUtf8("verticalLayout_4"));
+        verticalLayout_4->setContentsMargins(0, 0, 0, 0);
+        label_6 = new QLabel(widget1);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+
+        verticalLayout_4->addWidget(label_6);
+
+        label_13 = new QLabel(widget1);
+        label_13->setObjectName(QString::fromUtf8("label_13"));
+
+        verticalLayout_4->addWidget(label_13);
+
+        widget2 = new QWidget(groupBox);
+        widget2->setObjectName(QString::fromUtf8("widget2"));
+        widget2->setGeometry(QRect(130, 200, 81, 50));
+        verticalLayout_5 = new QVBoxLayout(widget2);
+        verticalLayout_5->setObjectName(QString::fromUtf8("verticalLayout_5"));
+        verticalLayout_5->setContentsMargins(0, 0, 0, 0);
+        label_7 = new QLabel(widget2);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+
+        verticalLayout_5->addWidget(label_7);
+
+        label_14 = new QLabel(widget2);
+        label_14->setObjectName(QString::fromUtf8("label_14"));
+
+        verticalLayout_5->addWidget(label_14);
+
+        widget3 = new QWidget(groupBox);
+        widget3->setObjectName(QString::fromUtf8("widget3"));
+        widget3->setGeometry(QRect(260, 200, 102, 50));
+        verticalLayout_6 = new QVBoxLayout(widget3);
+        verticalLayout_6->setObjectName(QString::fromUtf8("verticalLayout_6"));
+        verticalLayout_6->setContentsMargins(0, 0, 0, 0);
+        label_9 = new QLabel(widget3);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+
+        verticalLayout_6->addWidget(label_9);
+
+        label_15 = new QLabel(widget3);
+        label_15->setObjectName(QString::fromUtf8("label_15"));
+
+        verticalLayout_6->addWidget(label_15);
+
+        widget4 = new QWidget(groupBox);
+        widget4->setObjectName(QString::fromUtf8("widget4"));
+        widget4->setGeometry(QRect(20, 250, 123, 50));
+        verticalLayout_7 = new QVBoxLayout(widget4);
+        verticalLayout_7->setObjectName(QString::fromUtf8("verticalLayout_7"));
+        verticalLayout_7->setContentsMargins(0, 0, 0, 0);
+        label_10 = new QLabel(widget4);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+
+        verticalLayout_7->addWidget(label_10);
+
+        label_16 = new QLabel(widget4);
+        label_16->setObjectName(QString::fromUtf8("label_16"));
+
+        verticalLayout_7->addWidget(label_16);
+
+        widget5 = new QWidget(groupBox);
+        widget5->setObjectName(QString::fromUtf8("widget5"));
+        widget5->setGeometry(QRect(260, 250, 58, 36));
+        verticalLayout_8 = new QVBoxLayout(widget5);
+        verticalLayout_8->setObjectName(QString::fromUtf8("verticalLayout_8"));
+        verticalLayout_8->setContentsMargins(0, 0, 0, 0);
+        label_11 = new QLabel(widget5);
+        label_11->setObjectName(QString::fromUtf8("label_11"));
+
+        verticalLayout_8->addWidget(label_11);
+
+        label_17 = new QLabel(widget5);
+        label_17->setObjectName(QString::fromUtf8("label_17"));
+
+        verticalLayout_8->addWidget(label_17);
+
+
+        retranslateUi(contact);
+
+        QMetaObject::connectSlotsByName(contact);
     } // setupUi
 
-    void retranslateUi(QDialog *Contact)
+    void retranslateUi(QWidget *contact)
     {
-        Contact->setWindowTitle(QApplication::translate("Contact", "Contact Us", nullptr));
-        label->setText(QString());
-    
+        contact->setWindowTitle(QCoreApplication::translate("contact", "Form", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("contact", "CamelCase", nullptr));
+        label->setText(QCoreApplication::translate("contact", "<html><head/><body><p><img src=\":/img/img/CamelCaseIncLogo.png\"/></p></body></html>", nullptr));
+        label_2->setText(QCoreApplication::translate("contact", "Nicholas Moc", nullptr));
+        label_4->setText(QCoreApplication::translate("contact", "nmoc0@ivc.edu", nullptr));
+        label_3->setText(QCoreApplication::translate("contact", "Alan Truong", nullptr));
+        label_5->setText(QCoreApplication::translate("contact", "atruong44@ivc.edu", nullptr));
+        label_8->setText(QCoreApplication::translate("contact", "Ali Eshghi", nullptr));
+        label_12->setText(QCoreApplication::translate("contact", "aeshghi0@ivc.edu", nullptr));
+        label_6->setText(QCoreApplication::translate("contact", "Allison Chu", nullptr));
+        label_13->setText(QCoreApplication::translate("contact", "achu21@ivc.edu\n"
+"", nullptr));
+        label_7->setText(QCoreApplication::translate("contact", "Amin Parvizi", nullptr));
+        label_14->setText(QCoreApplication::translate("contact", "ami5n@me.com\n"
+"", nullptr));
+        label_9->setText(QCoreApplication::translate("contact", "Amirarsalan Valipour", nullptr));
+        label_15->setText(QCoreApplication::translate("contact", "avalipour1@ivc.edu\n"
+"", nullptr));
+        label_10->setText(QCoreApplication::translate("contact", "Issac Guido", nullptr));
+        label_16->setText(QCoreApplication::translate("contact", "iguido0@saddleback.edu\n"
+"", nullptr));
+        label_11->setText(QCoreApplication::translate("contact", "Sepehr Naji", nullptr));
+        label_17->setText(QCoreApplication::translate("contact", "TextLabel", nullptr));
     } // retranslateUi
 
 };
 
 namespace Ui {
-    class Contact: public Ui_Contact {};
+    class contact: public Ui_contact {};
 } // namespace Ui
 
 QT_END_NAMESPACE
